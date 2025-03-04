4d6da8b5175e432c7da9f13857275f389191da10
Elliot Lawrence <110320861+elaw142@users.noreply.github.com>
2025-03-04 14:57:18 +1300

    fix button fade in time

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
       document.getElementById("buttons").classList.add("visible");
-    }, 10);
+    }, 2500);
   });
   
